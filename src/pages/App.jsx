70eaaeaa900edb9b0634b381787e9133213bf484
Salman Abbas (سلیمان) <55852619+ItsSalman99@users.noreply.github.com>
2025-03-19 21:57:14 +0500

    ui changes

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import '../App.css'
 import Header from '../components/Header'
-import GlitchText from '../components/GlitchText.jsx';
+import MetaBalls from '../components/MetaBalls';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,16 +11,39 @@ function App() {
     <>
       <Header />
 
-      <div class="section-center ">
-        <h1 className='bg-white'>Hello, I'm Salman Abbas</h1>
-        <GlitchText
-          speed={1}
-          enableShadows={true}
-          enableOnHover={true}
-          className='custom-class'
-        >
-          Full-Stack Developer
-        </GlitchText>
+      {/* <SplashCursor /> */}
+
+
+      <div class="section-center m-4 p-4">
+        <div className="row">
+          <div className="col-8">
+            <div className='outline-div'>
+              <span class="outline">Hey, I am <span className='bold'>Salman</span> </span><br />
+              <span class="bold">Full-Stack <span className='outline'>Developer</span></span><br />
+              <span class="outline">Building Scalable</span> <br />
+              <span class="bold">& Efficient Web Solutions</span>
+
+            </div>
+          </div>
+          <div className="col-4">
+            <MetaBalls
+              color="#ffffff"
+              cursorBallColor="#ffffff"
+              cursorBallSize={2}
+              ballCount={22}
+              animationSize={25}
+              enableMouseInteraction={true}
+              enableTransparency={true}
+              hoverSmoothness={0.05}
+              clumpFactor={1}
+              speed={0.3}
+            />
+          </div>
+        </div>
+
+
+
+
       </div>
     </>
   )
