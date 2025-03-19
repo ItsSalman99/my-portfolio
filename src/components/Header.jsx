16cca8ef6bd2bc867e46660ea6110511dc7ba645
Salman Abbas (سلیمان) <55852619+ItsSalman99@users.noreply.github.com>
2025-03-19 21:56:51 +0500

    components changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,21 +5,23 @@ import '../css/header.css'
 function Header() {
     return (
         <>
-            <a href="#" class="logo">
-                <img src="https://assets.codepen.io/1462889/fcy.png" alt="External Image" />
-                
-            </a>
+            <div className='navbarDiv'>
+                <a href="#" class="logo">
+                    {/* <img src="https://assets.codepen.io/1462889/fcy.png" alt="External Image" /> */}
+                    Salman A.
+                </a>
 
-            <input class="menu-icon" type="checkbox" id="menu-icon" name="menu-icon" />
-            <label for="menu-icon"></label>
-            <nav class="nav">
-                <ul class="pt-5">
-                    <li><a href="#">Work</a></li>
-                    <li><a href="#">Studio</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
+                <input class="menu-icon" type="checkbox" id="menu-icon" name="menu-icon" />
+                <label for="menu-icon"></label>
+                <nav class="nav">
+                    <ul class="pt-5">
+                        <li><a href="#">Work</a></li>
+                        <li><a href="#">Studio</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
         </>
     )
 }
