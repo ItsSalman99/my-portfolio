63081b0c424097c53d1b0e66e4e7a7f38b1d9e61
Salman Abbas (سلیمان) <55852619+ItsSalman99@users.noreply.github.com>
2025-03-22 00:01:52 +0500

    ui changes

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import '../App.css'
 import Header from '../components/Header'
-import MetaBalls from '../components/MetaBalls';
+import Particles from '../components/Particles.jsx';
 
 
 function App() {
@@ -9,42 +9,39 @@ function App() {
 
   return (
     <>
+
+
+      <div >
+        <Particles
+          particleColors={['#000', '#000']}
+          particleCount={200}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
+      </div>
+
       <Header />
 
       {/* <SplashCursor /> */}
 
 
       <div class="section-center m-4 p-4">
-        <div className="row">
-          <div className="col-8">
-            <div className='outline-div'>
-              <span class="outline">Hey, I am <span className='bold'>Salman</span> </span><br />
-              <span class="bold">Full-Stack <span className='outline'>Developer</span></span><br />
-              <span class="outline">Building Scalable</span> <br />
-              <span class="bold">& Efficient Web Solutions</span>
+        <div className='outline-div'>
+          <span class="outline ">Hey, I am <span className='bold'>Salman</span> </span><br />
+          <span class="bold">Full-Stack <span className='outline'>Developer</span></span><br />
+          <span class="outline">Building Scalable</span> <br />
+          <span class="bold">& Efficient Web Solutions</span>
 
-            </div>
-          </div>
-          <div className="col-4">
-            <MetaBalls
-              color="#ffffff"
-              cursorBallColor="#ffffff"
-              cursorBallSize={2}
-              ballCount={22}
-              animationSize={25}
-              enableMouseInteraction={true}
-              enableTransparency={true}
-              hoverSmoothness={0.05}
-              clumpFactor={1}
-              speed={0.3}
-            />
-          </div>
         </div>
 
 
 
 
-      </div>
+      </div >
     </>
   )
 }
